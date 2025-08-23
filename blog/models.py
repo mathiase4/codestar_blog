ac530863ae9cde957e23a873a9f8004433d98db0
mathiase4 <mathiaseriksson98@hotmail.com>
2025-08-23 13:48:13 +0200

    fixed str__ f comment

--- blog/models.py
+++ blog/models.py
@@ -20,7 +20,7 @@ class Post(models.Model):
         ordering = ["-created_on"]
 
     def __str__(self):
-        return f"{self.title} | written by {self.author}"
+        return f"Comment {self.title} by {self.author}"
    
 
             
