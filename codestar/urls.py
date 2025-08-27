9fc984595aea3bdf66ac3538bd8d124103a1ad5d
mathiase4 <mathiaseriksson98@hotmail.com>
2025-08-27 19:39:40 +0200

    generic post view

--- codestar/urls.py
+++ codestar/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from blog import views
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('blog/', views.home),
-    
+    path("", include("blog.urls"), name="blog-urls"),
 ]
+
