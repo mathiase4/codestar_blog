223fa15800e4b8f177ec6a52eb0453ae966b44c9
mathiase4 <mathiaseriksson98@hotmail.com>
2025-09-01 10:52:57 +0200

    added css to base html

--- blog/admin.py
+++ blog/admin.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-
-class PostAdmin(admin.ModelAdmin):
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
     list_display = ("title", "author", "status", "created_on")
     list_filter = ("status", "created_on", "author")
     search_fields = ("title", "content")
     prepopulated_fields = {"slug": ("title",)}
     ordering = ("-created_on",)
-
+    summernote_fields = ('content',)
+    
 
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Comment)
